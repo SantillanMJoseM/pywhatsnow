e72f9c8f324385aedc5d9529ae91a7cbcf989ed2
Santillan Medina Jose Maria <josemedina111@gmail.com>
2023-10-30 19:56:15 -0300

    Se quita numero de test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pywhatsnow as py
 
-py.sendwhatdoc_immediately('+543854067129','Resumen_19_1.jpg', 'Hola como estas', 9, True, 5)
-py.sendwhatdoc_immediately('+543854067129','Resumen_20_1.jpg','Hola como estas', 9, True, 5)
+py.sendwhatdoc_immediately('+','Resumen_19_1.jpg', 'Hola como estas', 9, True, 5)
+py.sendwhatdoc_immediately('+','Resumen_20_1.jpg','Hola como estas', 9, True, 5)
 
 
